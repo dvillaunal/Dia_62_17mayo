b83949452ecaadb7897717da9cde950510a84ea6
DVILLA <dvilla@unal.edu.co>
2021-05-17 22:44:20 -0500

    Reto diario

--- main.r
+++ main.r
@@ -1,21 +1,17 @@
-```{r, eval=FALSE, include=TRUE}
-"Protocolo:
+## ---- eval=FALSE, include=TRUE-----------------------------------------
+## "Protocolo:
+## 
+##  1. Daniel Felipe Villa Rengifo
+## 
+##  2. Lenguaje: R
+## 
+##  3. Tema: Cree funciones SIN argumentos en R con cadenas de texto y con valores numéricos  (realice al menos dos ejercicios que requieran cargar archivos externos *.csv cada uno con al menos 50 filas y tres datos por fila, lea y procese la información del archvo leído, y guarde las respuestas a los ejercicios  en archivos independientes tipo *.txt)
+## 
+##  4. Fuentes:
+##     https://www.generatedata.com"
 
- 1. Daniel Felipe Villa Rengifo
 
- 2. Lenguaje: R
-
- 3. Tema: Cree funciones SIN argumentos en R con cadenas de texto y con valores numéricos  (realice al menos dos ejercicios que requieran cargar archivos externos *.csv cada uno con al menos 50 filas y tres datos por fila, lea y procese la información del archvo leído, y guarde las respuestas a los ejercicios  en archivos independientes tipo *.txt)
-
- 4. Fuentes:
-    https://www.generatedata.com"
-```
-
-# Ejercicio 1°
-
-La empresa ducales le han querido robar su toque secreto y para eso han hackeado un correo que termina en @AOL.com, extrayendo el pin de verificación a la a receta, por ende migraremos la base de datos de correos a @gmail.com y cambiaremos su Pin de verificación
-
-```{r}
+## ----------------------------------------------------------------------
 # Importamos la base de datos:
 base1 <- read.csv(file ="Base1.csv", header = T, sep = ",")
 print(base1)
@@ -28,11 +24,9 @@ f1 <- function(){
 f2 <- function(){
   return(base1$Correo <- as.factor(base1$Correo))
 }
-```
 
-Ahora vamos a filtrar la empresa ducales, para despues volverla a filtrar por correo `@AOL.com`
 
-```{r}
+## ----------------------------------------------------------------------
 # Filtremos la base de datos por Ducales:
 galleta <- dplyr::filter(base1, Empresa == "Ducales")
 print(galleta)
@@ -54,14 +48,9 @@ email$Pin <- c(s(), s(), s(), s(), s(), s(), s())
 
 ## Ahora exportamos los resultados:
 write.table(email, file = "DucalesSegura.txt", sep = " | ", row.names = F)
-```
 
 
-# Ejercicio 2
-
-Una base de datos en dolares americanos desea subir a los pensionados que son viud@s en 100.00 dolares
-
-```{r}
+## ----------------------------------------------------------------------
 # Importamos la base de datos:
 base2 <- read.csv(file = "Base2.csv", sep = ",", dec = ".", header = T)
 print(base2)
@@ -80,11 +69,9 @@ level <- function(){
 }
 
 level()
-```
 
 
-
-```{r}
+## ----------------------------------------------------------------------
 # Filtremos la base por los peensionados:
 pensionados <- dplyr::filter(base2, Pension == "Yes")
 
@@ -98,10 +85,14 @@ mas <- function(){
 
 # ahora sus pensiones estan en valor colombiano:
 pensionados$Valor <- mas()
-```
 
-```{r}
+
+## ----------------------------------------------------------------------
 ##Exportamos el resultado:
 write.table(pensionados, file = "PensionCOP.txt", sep = ",", dec = ".", row.names = F)
-```
+
+
+## ---- eval=FALSE, include=FALSE----------------------------------------
+## library(knitr)
+## purl("Dia_62_17mayo.Rmd", "main.r")
 
